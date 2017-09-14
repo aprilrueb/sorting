1e7f24d50bc15ada260744c4147f7c0bc442f9f0
April Rueb <aprilrueb@gmail.com>
2017-09-14 12:41:44 -0400

    bubble sort specs passing

---
+++ bubblesort.js
@@ -0,0 +1,23 @@
+function bubbleSort(array) {
+  var notDone = true;
+  while(notDone){
+    notDone = false;
+    for(var i=0; i<array.length; i++){
+      if(array[i] > array[i+1]){
+        bubbleSort.swap(array, i, i+1);
+        // array[i] = array[i+1];
+        // array[i+1] = temp;
+        notDone = true;
+      } else {
+        continue;
+      }
+    }
+  }
+  return array;
+}
+
+bubbleSort.swap = function(array, index1, index2){
+  var temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,22 @@
+describe('Bubble Sort', function(){
+
+  beforeAll(function () {
+    spyOn(bubbleSort, 'swap').and.callThrough();
+  });
+
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+    expect(bubbleSort.swap.calls.count()).toEqual(0);
+  });
+
+
+  it('handles an array with one item', function(){
+    expect( bubbleSort([5]) ).toEqual( [5] );
+    expect(bubbleSort.swap.calls.count()).toEqual(0);
+  });
+
+  it('handles an array with multiple items', function(){
+    expect( bubbleSort([3,2,1]) ).toEqual( [1,2,3] );
+    expect(bubbleSort.swap.calls.count()).toEqual(3);
+  });
+});
